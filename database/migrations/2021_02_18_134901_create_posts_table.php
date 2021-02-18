5327173cba8c5ff4314bc38254ffbf8ae196464a
Cristian <cristianmihai193@gmail.com>
2021-02-18 17:05:53 +0100

    info_post

---
+++ database/migrations/2021_02_18_134901_create_posts_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+            $table->string('title', 150);
+            $table->string('subtitle', 250);
+            $table->mediumtext('text');
+            $table->string('author', 100);
+            $table->string('img_path', 300);
+            $table->date('publication_date');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+}
---
+++ database/seeds/PostsTableSeeder.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Post;
+use Faker\Generator as Faker;
+
+class PostsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run(Faker $faker)
+    {
+        for ($i = 0; $i < 10; $i++) {
+            // creiamo una nuova istanza 
+            $newPost = new Post();
+
+            //aggiungiamo i dati fake
+            $newPost->title = $faker->sentence(7);
+            $newPost->subtitle = $faker->sentence(14);
+            $newPost->text = $faker->text(7000);
+            $newPost->author = $faker->name;
+            $newPost->img_path = $faker->imageUrl(640, 480, 'cats');
+            $newPost->publication_date = $faker->dateTime();
+
+            // salviamo i dati
+            $newPost->save();
+
+        }
+    }
+}
